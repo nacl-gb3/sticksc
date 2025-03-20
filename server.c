28d2da745d70b1c2c076db634490e04f4ccd194c
nacl-gb3 <67475811+nacl-gb3@users.noreply.github.com>
2025-03-19 21:22:27 -0500

    Added AGPL disclaimer to source files

--- server.c
+++ server.c
@@ -1,3 +1,22 @@
+/*
+    sticksc - pseudo p2p implementation of sticks/chopsticks game
+    Copyright (C) 2025 nacl-gb3 (Gary Bond III)
+    GitHub Link: https://github.com/nacl-gb3/sticksc/tree/main
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "server.h"
 #include "error.h"
 #include "game.h"
